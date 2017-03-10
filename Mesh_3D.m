ba16987e1cc72d0193d81fa362fddcc94e8353da
Catherine <cat.taylor.taylor@btinternet.com>
2017-03-10 15:01:11 +0000

    Basic 3D deformation algorithm written.

--- Mesh_3D.m
+++ Mesh_3D.m
@@ -7,22 +7,23 @@ FV = obj.f.v;
 V = obj.v;
 h=figure;
 trimesh(FV(:,1:3), V(:,1), V(:,2), V(:,3)); 
+axis([-1.5 1.5 -1.5 1.5 -1.5 1.5])
+w=1000;
+ [x,y,z] = ginput(4);
 
-% [x,y,z] = ginput(4);
-
-pts = zeros(4,3);
-datacursormode on
-dcm_obj = datacursormode(h);
-
-for i=1:4
-    waitforbuttonpress;
-    f = getCursorInfo(dcm_obj);
-    pts(i,:) = f.Position;
-end
-
-x = pts(:,1);
-y=pts(:,2);
-z=pts(:,3);
+% pts = zeros(4,3);
+% datacursormode on
+% dcm_obj = datacursormode(h);
+% 
+% for i=1:4
+%     waitforbuttonpress;
+%     f = getCursorInfo(dcm_obj);
+%     pts(i,:) = f.Position;
+% end
+% 
+% x = pts(:,1);
+% y=pts(:,2);
+% z=pts(:,3);
 v = zeros(3,1);
 for i =1:3
     min_dist= 10000; 
@@ -42,6 +43,7 @@ end
 
 hold on
 plot3(x,y,z,'o');
+
 d = zeros(length(V),1);
 D=zeros(length(V));
 A = zeros(length(V));
@@ -76,3 +78,66 @@ end
 
 L = eye(length(V)) - D\A;
 delta = L*V(:, 1:3);
+
+A1 = zeros(3*length(V)+9, 3*length(V));
+b1 = zeros(3*length(V)+9,1);
+for i=1:length(V)
+    A1(3*(i-1)+1, 3*(i-1)+1) = 1;
+    A1(3*(i-1)+2, 3*(i-1)+2) = 1;
+    A1(3*(i-1)+3, 3*(i-1)+3) = 1;
+    b1(3*(i-1)+1) = delta(i,1);
+    b1(3*(i-1)+2) = delta(i,2);
+    b1(3*(i-1)+3) = delta(i,3);
+   for j =1: d(i)
+       A1(3*(i-1)+1, 3*(Vertex_neighbours{i}(j)-1)+1) = -1/d(i);
+       A1(3*(i-1)+2, 3*(Vertex_neighbours{i}(j)-1)+2) = -1/d(i);
+       A1(3*(i-1)+3, 3*(Vertex_neighbours{i}(j)-1)+3) = -1/d(i);
+   end
+end
+
+   for i=1:3
+    A1(3*(i-1)+3*length(V)+1, 3*(v(i)-1)+1) = 1;
+    A1(3*(i-1)+2+3*length(V), 3*(v(i)-1)+2) = 1;
+    A1(3*(i-1)+3+3*length(V), 3*(v(i)-1)+3) = 1; 
+   end
+    b1(3*length(V)+1) = x(4);
+    b1(3*length(V)+2) = y(4);
+    b1(3*length(V)+3) = z(4);
+   for i=2:3
+    b1(3*length(V)+3*(i-1)+1) = x(i);
+    b1(3*length(V)+3*(i-1)+2) = y(i);
+    b1(3*length(V)+3*(i-1)+3) = z(i);
+   end
+   
+   V1 = (A1'*A1)\A1'*b1;
+   
+  Vn=zeros(length(V),3);
+for i =1:length(V1)
+    if (mod(i,3)==1)
+        Vn((i+2)/3, 1) = V1(i);
+    elseif (mod(i,3)==2)
+        Vn((i+1)/3,2) = V1(i);
+    else
+            Vn(i/3,3) = V1(i);
+    end    
+end
+
+figure;
+trimesh(FV(:,1:3), Vn(:,1), Vn(:,2), Vn(:,3)); 
+hold on
+plot3(x,y,z,'o');
+axis([-1.5 1.5 -1.5 1.5 -1.5 1.5])
+
+% for i =1:length(V)
+%     Ai = zeros(3*length(Vertex_neighbours{i}), 7);
+%     bi = zeros(3*length(Vertex_neighbours{i}), 1);
+%     for j =1: length(Vertex_neighbours{i})
+%         Ai(3*(j-1)+1, :) = [V(Vertex_neighbours{i}(j),1), 0, V(Vertex_neighbours{i}(j),3), -V(Vertex_neighbours{i}(j),2),1,0,0];
+%         Ai(3*(j-1)+2, :) = [V(Vertex_neighbours{i}(j),2), -V(Vertex_neighbours{i}(j),3), 0 ,V(Vertex_neighbours{i}(j),1),0,1,0];
+%         Ai(3*(j-1)+3, :) = [V(Vertex_neighbours{i}(j),3), V(Vertex_neighbours{i}(j),2), -V(Vertex_neighbours{i}(j),1),0,0,0,1];
+%         bi(3*(j-1)+1) =     V(Vertex_neighbours{i}(j),1); %WRONG
+%         bi(3*(j-1)+2) =     V(Vertex_neighbours{i}(j),2);
+%         bi(3*(j-1)+3) =     V(Vertex_neighbours{i}(j),3);
+%     end
+%     Ti = (Ai'*Ai)\Ai'*bi;
+% end
